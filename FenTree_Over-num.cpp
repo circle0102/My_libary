95d4ec602c75a6c5c15469a98303f2072d74fe31
circle0102 <ciyqularisch@gmail.com>
2025-04-18 17:41:44 +0900

    Update FenTree_Over-num.cpp

--- FenTree_Over-num.cpp
+++ FenTree_Over-num.cpp
@@ -4,68 +4,60 @@ struct fenwick_tree {
     vector<T> data;
     int N = 0;
 
-    fenwick_tree() {
-        data.resize(0, 0);
-    }
+    fenwick_tree() {}
+    fenwick_tree(int n) : N(n), data(n, 0) {}
 
-    fenwick_tree(int n) {
-        N = n;
-        data.resize(n, 0);
-    }
-
+    // i 番目（0-indexed）に x を加算
     void add(int i, T x) {
         assert(0 <= i && i < N);
-        i++;
-        while (i <= N) {
-            data[i - 1] += x;
-            i += i & -i;
-        }
+        for (++i; i <= N; i += i & -i) data[i-1] += x;
     }
 
-    T sum(int r) {
+    // [0, r) の和
+    T sum(int r) const {
         assert(0 <= r && r <= N);
         T s = 0;
         while (r > 0) {
-            s += data[r - 1];
+            s += data[r-1];
             r -= r & -r;
         }
         return s;
     }
 
-    T sum(int l, int r) {
+    // [l, r) の和
+    T sum(int l, int r) const {
         assert(0 <= l && l <= r && r <= N);
         return sum(r) - sum(l);
     }
 
-    T get(int i) {
-        return sum(i + 1) - sum(i);
+    // i 番目の値を取得
+    T get(int i) const {
+        return sum(i+1) - sum(i);
     }
 
+    // i 番目の値を x にセット
     void set(int i, T x) {
         add(i, x - get(i));
     }
 
-    T operator[](int idx) {
-        return get(idx);
-    }
-
-    // 転倒数を計算：配列 a の中の転倒数（0-indexed, 要座圧）
-    static long long count_inversions(vector<int> a) {
+    // 転倒数を数える静的メソッド
+    // 配列 a の転倒数を O(n log n) で返す
+    static long long inversion_count(vector<T> a) {
         int n = a.size();
-        // 値の範囲が広い場合は座標圧縮が必要
-        vector<int> b = a;
-        sort(b.begin(), b.end());
-        b.erase(unique(b.begin(), b.end()), b.end());
-        for (int &x : a) {
-            x = lower_bound(b.begin(), b.end(), x) - b.begin(); // 座圧
-        }
+        // 座標圧縮
+        vector<T> xs = a;
+        sort(xs.begin(), xs.end());
+        xs.erase(unique(xs.begin(), xs.end()), xs.end());
 
-        // BITで転倒数を数える
-        fenwick_tree<int> bit(b.size());
+        // 圧縮後の値域で BIT を用意
+        fenwick_tree<long long> bit(xs.size());
+
         long long inv = 0;
+        // 右から左へスキャンし、「より小さい要素の個数」を足し合わせ
         for (int i = n - 1; i >= 0; --i) {
-            inv += bit.sum(a[i]);  // a[i] 未満の数の個数
-            bit.add(a[i], 1);      // a[i] を1個追加
+            int idx = int(lower_bound(xs.begin(), xs.end(), a[i]) - xs.begin());
+            inv += bit.sum(idx);  // 圧縮値 < idx の要素数
+            bit.add(idx, 1);
         }
         return inv;
     }
