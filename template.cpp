0fdd9869356d468e930f31704afbc6a7b7eccdc9
circle0102 <ciyqularisch@gmail.com>
2025-04-18 15:53:59 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -128,7 +128,193 @@ class Timer{
         return (this -> count())<x;
     }
 };
-//---------------------------------------------------------------
+//累世和---------------------------------------------------------------
+template<typename T> struct sum_vec{
+    vector<T> data;
+    vector<T> sum;
+    int N;
+    sum_vec(const vector<T> &vec){
+        int N=vec.size();
+        data.resize(N);
+        sum.resize(N+1,0);
+        for(int i=0;i<N;i++){
+            data[i]=vec[i];
+            sum[i+1]=sum[i]+data[i];
+        }
+    }
+    sum_vec(int n){
+        N=n;
+        data.resize(N,0);
+        sum.resize(N,0);
+    }
+    sum_vec(){
+        N=0;
+    }
+    void cal(){
+        sum[0]=0;
+        for(int i=0;i<N;i++){
+            sum[i+1]=data[i]+sum[i];
+        }
+        return;
+    }
+    T cul_sum(int l,int r){
+        return sum[r]-sum[l];
+    }
+    void push_back(T x){
+        data.push_back(x);
+        T b=sum.back();
+        sum.push_back(b+x);
+        N++;
+    }
+    void pop_back(){
+        assert(1<=(int)data.size());
+        data.pop_back();
+        sum.pop_back();
+    }
+};
+//Mod Int----------------------------------------------------------------
+struct modint{
+    using ll=long long;
+    ll mod=998244353;
+    ll num=0;
+    modint(){
+        mod=998244353;
+        num=0;
+    }
+    modint(ll n){
+        num=n;
+        mod=998244353;
+    }
+    modint(ll n,ll m){
+        mod=m;
+        num=n;
+    }
+    operator long long(){
+        return num;
+    }
+    void operator=(ll n){
+        num=n;
+    }
+    void operator+=(ll add){
+        num+=add;
+    }
+    void operator-=(ll sub){
+        num-=sub;
+    }
+    void operator*=(ll tim){
+        num*=tim%mod;
+        num%=mod;
+    }
+    bool operator==(ll other){
+        return num==other%mod;
+    }
+    bool operator!=(ll other){
+        return num!=other%mod;
+    }
+    bool operator<(ll other){
+        return num<other;
+    }
+    
+};
+//にぶたん-------------------------------------------------------------------------
+template<class T> 
+int search_min(vector<T> &vec, bool(*check)(T)){
+    int N=(int)vec.size();
+    int ok=N;
+    int ng=-1;
+    while(abs(ok-ng)>1){
+        int mid=(ok+ng)/2;
+        if(check(vec[mid])){
+            ok=mid;
+        }else{
+            ng=mid;
+        }
+    }
+    return ok;
+}
+template<class T>
+int search_max(vector<T> &vec,bool(*check)(T)){
+    int ok=-1;
+    int ng=(int)vec.size();
+    while(abs(ok-ng)>1){
+        int mid=(ok+mid)/2;
+        if(check(mid)){
+            ok=mid;
+        }else{
+            ng=mid;
+        }
+    }
+    return ok;
+}
+//以下半開区間[l,r)についての二分探索
+ll search_min(ll l,ll r,bool(*check)(ll)){
+    ll ok=r;
+    ll ng=l-1;
+    while(abs(ok-ng)>1){
+        ll mid=ok/2+ng/2;
+        if(check(mid)){
+            ok=mid;
+        }else{
+            ng=mid;
+        }
+    }
+    return ok;
+}
+ll search_max(ll l,ll r,bool(*check)(ll)){
+    ll ok=l-1;
+    ll ng=r;
+    while(abs(ok-ng)){
+        ll mid=ok/2+ng/2;
+        if(check(mid)){
+            ok=mid;
+        }else{
+            ng=mid;
+        }
+    }
+    return ok;
+}
+int comp;
+bool check(int ai){
+    if(comp<=ai){
+        return true;
+    }
+    return false;
+}
+//確率pでtrueを返す関数--------------------------------------------------------
+bool prob(double p){
+  static mt19937_64 mt64(0);
+  uniform_real_distribution<double> uni(0.0, 1.0);
+  if(p>=uni(mt64)){
+    return true;
+  }
+  return false;
+}
+//乱数生成-------------------------------------------------------------
+class Random_Gen{
+    random_device seed_gen;
+    mt19937 engine;
+    uniform_int_distribution<int64_t> dist;
+    public:
+        // Constructor [l,r]で生成する値の範囲を指定
+        Random_Gen() : engine(seed_gen()) {}
+        Random_Gen(int64_t l, int64_t r) : engine(seed_gen()), dist(l,r) {}
+        
+        // 現在の生成する値の範囲をstd::pairで返す
+        pair<int64_t,int64_t> get_range(){
+            return make_pair(dist.min(),dist.max());
+        }
+        // 生成する値の範囲を[l,r]に変更する
+        void set_range(int64_t l, int64_t r){
+            uniform_int_distribution<int64_t>::param_type Param(l,r);
+            dist.param(Param);
+        }
+        // [l,r]内の一様分布の整数を返す
+        int64_t gen(){
+            return dist(engine);
+        }
+        int64_t operator()(){ return gen(); }
+};
+//-------------------------------------------------------------------
 int main(){
     ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
