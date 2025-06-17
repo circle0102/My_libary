5dc8261aeeaee09d08134d37d66c6f9d89db378b
circle0102 <ciyqularisch@gmail.com>
2025-06-17 09:03:23 +0900

    Create header.cpp

---
+++ header.cpp
@@ -0,0 +1,42 @@
+//thair library is imported by 
+//https://github.com/circle0102/My_libary
+//https://github.com/jiyujin816/library
+//https://nyaannyaan.github.io/library/
+#pragma GCC optimize("O3")
+#ifdef __LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <numeric>
+#include <limits>
+#include <iterator>
+#include <cassert>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <random>
+#include <sstream>
+#include <chrono>
+#include <stdexcept>
+#include <set>
+#include <map>
+//#include <atcoder/all>
+using endl="\n";
+using namespace std;
+using ll=long long;
+#define rep(i,n) for(ll i=0;i<n;++i)
+#define reph(i,n) for(ll i=0;i<=n;++i)
+#define repr(i,a,b) for(ll i=a;i<b;++i)
+#define reprh(i,a,b) for(ll i=a;i<=b;++i)
+int main(){
+  
+}
